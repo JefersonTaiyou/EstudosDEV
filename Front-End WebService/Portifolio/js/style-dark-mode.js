a861c9a1eaa1678ff16e1dfeb7f1d0ff9c81f20b
Jeferson Sabino dos Santos Junior <86162489+JefersonTaiyou@users.noreply.github.com>
2023-03-04 09:28:33 -0300

    Update style-dark-mode.js

--- Front-End WebService/Portifolio/js/style-dark-mode.js
+++ Front-End WebService/Portifolio/js/style-dark-mode.js
@@ -1,9 +1,9 @@
-/* ========================== toggle style switcher =========================== */
+
 const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
 styleSwitcherToggle.addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
 })
-// hide style - switcher on scroll
+
 window.addEventListener("scroll", () => {
     if(document.querySelector(".style-switcher").classList.contains("open"))
     {
